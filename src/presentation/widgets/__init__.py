86aea885b53096705a0aab17d6e93bb2c950f54e
Art <shai.ru1@yandex.ru>
2026-04-18 21:07:20 +0300

    return export

--- src/presentation/widgets/__init__.py
+++ src/presentation/widgets/__init__.py
@@ -1,14 +1,12 @@
 
 from .selectable_dxf_tree_handler import SelectableDxfTreeHandler
 from .viewer_dxf_tree_handler import ViewerDxfTreeHandler
-from .db_tree_handler import DbTreeWidgetHandler
 from .qgis_layer_sync_manager import QGISLayerSyncManager
 from .svg_preview_dialog import SvgPreviewDialog
 
 __all__ = [
     'SelectableDxfTreeHandler',
     'ViewerDxfTreeHandler',
-    'DbTreeWidgetHandler',
     'QGISLayerSyncManager',
     'SvgPreviewDialog',
 ]
